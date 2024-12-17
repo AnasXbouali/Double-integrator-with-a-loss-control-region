fed4bbc54fe3835bda7d69f1a2c3ae9258e87b88
Anas Bouali <anassbouali1998@gmail.com>
2024-12-17 11:30:54 +0100

    new

---
+++ example1_2.jl
@@ -0,0 +1,92 @@
+using OptimalControl
+using NLPModelsIpopt
+using Plots
+using Plots.PlotMeasures
+using Roots
+
+function F(x, y, k=130)
+    return 1 / (1 + exp(-k * (y - x)))
+end
+
+ε  = 1e-4 
+
+# First example: intial condition (-1,2)
+DI = @def begin
+
+    tf ∈ R, variable
+    t ∈ [0., tf], time
+    q = [x1, x2, λ] ∈ R^3, state
+    ω = [u,v] ∈ R^2, control
+
+    tf ≥ 0
+    
+    x1(0)  ==  -1
+    x2(0)  ==   2
+    x1(tf) == 0.0
+    x2(tf) == 0.0
+
+    -1 ≤ u(t) ≤ 1
+    -1 ≤ λ(t) ≤ 1
+    
+    q̇(t) == [x2(t),F(x1(t), x2(t))*u(t) + (1-F(x1(t), x2(t)))*λ(t), F(x1(t), x2(t))*v(t)]
+    
+    tf + ∫(ε*(v(t))^2 + (1-F(x1(t), x2(t)))*(u(t))^2) → min
+end
+
+sol1 = solve(DI; init = (state = t -> [0.1, 0.1, 0.1], control = [0.1,0.1], variable = 25), grid_size=50)
+sol2 = solve(DI; init = sol1, grid_size=100)
+sol3 = solve(DI; init = sol2, grid_size=200)
+sol4 = solve(DI; init = sol3, grid_size=300)
+sol5 = solve(DI; init = sol4, grid_size=400)
+sol6 = solve(DI; init = sol5, grid_size=500)
+sol  = solve(DI; init = sol6, grid_size=1000, print_level=4)
+
+# Extract solution
+tf   = sol.variable
+y1(t)= sol.state(t)[1]
+y2(t)= sol.state(t)[2]
+plt = plot(y1, y2, 0, tf, color="blue", label=false)
+
+# Plot states
+x = range(-3., 3., length=200)
+y = range(-3., 3., length=200)
+Z = [y > x for x in x, y in y]
+heatmap!(plt, x, y, Z, color=[:lightgreen, :red2], fillalpha=0.4, colorbar=false)
+plot!(
+    x -> x,  
+    color = :black,
+    label = false,
+    linewidth = 2)
+A = plot!(y1, y2, 0, tf, color="blue", lw=1.5, label="optimal trajectory")
+
+# Plot control
+u(t)= sol.control(t)[1]
+λ(t)= sol.state(t)[3]
+diff_func(t) = y2(t) - y1(t)
+t_cross1 = find_zero(diff_func, (0, 3), Bisection())
+t_cross2 = find_zero(diff_func, (5, 7.5), Bisection())
+function control(t)
+    if t <= t_cross1
+        return u(t)
+    elseif t_cross1 <= t <= t_cross2
+        return λ(t)
+    elseif t_cross2 <= t <= tf
+        return u(t)
+    end 
+end
+B = plot(control, 0,tf,color="red", lw=1.5, label="optimal control")
+
+# Plot costates
+q1(t)= sol.costate(t)[1]
+q2(t)= sol.costate(t)[2]
+plot(q1, 0,tf, color="purple4", lw=1.5, label="costate p1")
+C = plot!(q2, 0,tf, color="mediumorchid1", lw=1.5, label="costate p2")
+
+plot(A,B,C,layout=(1, 3), size=(1600,600))
+
+
+
+
+
+
+
---
+++ example2_2.jl
@@ -0,0 +1,111 @@
+using OptimalControl
+using NLPModelsIpopt
+using Plots
+using Plots.PlotMeasures
+using Roots
+
+function G(x, y, k=100)
+    return 1 / (1 + exp(-k * ((x-2)^2 + (y+2)^2 - 1)))
+end
+x = range(-1,4, length=200)
+y = range(-5,1, length=200)
+
+z1 = [G(xi, yi) for xi in x, yi in y]
+p1=heatmap(x, y, z1', color=:viridis, aspect_ratio=:equal, xlabel="x", ylabel="y") 
+
+
+DI = @def begin
+
+    tf ∈ R, variable
+    t ∈ [0., tf], time
+    q = [x1, x2, λ] ∈ R^3, state
+    ω = [u,v] ∈ R^2, control
+
+    tf ≥ 0
+    
+    x1(0)  ==  0
+    x2(0)  ==  3
+    x1(tf) == 0.0
+    x2(tf) == 0.0
+    
+    -1 ≤ u(t) ≤ 1
+    -1 ≤ λ(t) ≤ 1
+    
+    q̇(t) == [x2(t),G(x1(t), x2(t))*u(t) + (1-G(x1(t), x2(t)))*λ(t), G(x1(t), x2(t))*v(t)]
+    
+    tf + ∫( ε*(v(t))^2 + (1-G(x1(t), x2(t)))*(u(t))^2 ) → min
+end
+
+sol1_ = solve(DI; init = (state = t -> [0.1, 0.1, 1], control = [1,0], variable = 30), grid_size=50)
+sol2_ = solve(DI; init = sol1_, grid_size=100)
+sol3_ = solve(DI; init = sol2_, grid_size=200)
+sol4_ = solve(DI; init = sol3_, grid_size=300)
+sol5_ = solve(DI; init = sol4_, grid_size=400)
+sol6_ = solve(DI; init = sol5_, grid_size=500)
+sol_  = solve(DI; init = sol6_, grid_size=1500)
+
+tf_   = sol_.variable
+y1_(t)= sol_.state(t)[1]
+y2_(t)= sol_.state(t)[2]
+p_ = plot(y1_, y2_, 0, tf_, color="blue", label=false)
+
+
+# Create the plot
+plt_ = plot(
+    xlims=(-1,6), 
+    ylims=(-3.5,3.5),
+    xlabel = "x1",
+    ylabel = "x2")
+
+x_range = range(-20, 8, length=1000)
+y_range = range(-20, 8, length=1000)
+
+xs = repeat(x_range, 1, length(y_range))
+ys = repeat(y_range', length(x_range), 1)
+
+function in_circle(x, y, center_x, center_y, radius)
+    return (x - center_x)^2 + (y - center_y)^2 ≤ radius^2
+end
+
+circle_center = (2, -2)
+circle_radius = 1
+
+θ = range(0, 2π, length=100)
+circle_x = circle_center[1] .+ circle_radius .* cos.(θ)
+circle_y = circle_center[2] .+ circle_radius .* sin.(θ)
+
+plot!(plt_, [-14, 6, 6, -14, -14], [-5, -5, 4, 4, -5], 
+      fill=true, fillcolor=:lightgreen, fillalpha=0.4, 
+      label=false, linecolor=nothing)
+
+plot!(plt_, circle_x, circle_y, fill=true, fillcolor=:white, fillalpha=1, 
+      label=false, linecolor=:black, linewidth=1)
+plot!(plt_, circle_x, circle_y, fill=true, fillcolor=:red2, fillalpha=0.4, 
+label=false, linecolor=:black, linewidth=1)
+
+AA = plot!(plt_, y1_, y2_, 0, tf_, color="blue", lw=1.5, label="optimal trajectory")
+
+diff_func_(t) = (y1_(t)-2)^2 + (y2_(t)+2)^2 - 1
+t_cross_ = find_zero(diff_func_, (4, 5), Bisection())
+t_cross2_ = find_zero(diff_func_, (5,6), Bisection())
+
+u_(t) = sol_.control(t)[1]
+λ0(t)= sol_.state(t)[3]
+function control_(t)
+    if t <= t_cross_
+        return u_(t)
+    elseif t_cross_ <= t <= t_cross2_
+        return λ0(t)
+    elseif t_cross2_ <= t 
+        return u_(t)
+    end
+end
+BB = plot(control_, 0, tf_, color="red", lw=1.5, label="optimal control")
+
+q1_(t)= sol_.costate(t)[1]
+q2_(t)= sol_.costate(t)[2]
+
+
+plot(q1_, 0,tf_, color="purple4", lw=1.5, label="costate p1")
+CC = plot!(q2_, 0,tf_, color="mediumorchid1", lw=1.5, label="costate p2")
+plot(AA, BB, CC, layout=(1, 3), size=(1600,600))
